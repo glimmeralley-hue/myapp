f89d042df3e9537c0f4afad72f2863450578a94d
glimmeralley-hue <glimmeralley@gmail.com>
2026-03-17 14:24:05 +0300

    new

--- sokogardenapp/src/components/Addproducts.jsx
+++ sokogardenapp/src/components/Addproducts.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import Loader from "./Loader";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 const Addproducts= () =>{
-//introduce hooks
+//introduce hooks(1)
 
     const [product_name,setProductname] = useState("");
     const[product_description,setProductdescription]= useState("");
@@ -11,24 +10,26 @@ const Addproducts= () =>{
     const [product_photo,setProductphoto]= useState("");
 
 
-    //set hooks for app states
+    //set hooks for app states (2)
 
     const[ loading,setLoading]= useState(false);
     const[success,setSuccess]=  useState("");
     const[error,setError] = useState("");
 
-     //below we have the useNavigate hook to redirect us to another page on success
+    
 
    
 
-    //create a function to handle submit
+    //create a function to handle submit -asynchronous arrow function(3)
 
     const handleSubmit = async(e)=>{
-        //prevent the site from reloading
+        //prevent the site from reloading  so that details entered are not  removed(4)
         e.preventDefault();
          
-        //set loading hook wirth a message(activate it)
+        //set loading hook with a message(activate it) (5)
         setLoading(true)
+
+        //append the details (8)
 
         try{
  const formdata = new FormData();
@@ -36,26 +37,38 @@ const Addproducts= () =>{
             formdata.append("product_description", product_description)
             formdata.append("product_cost",product_cost);
             formdata.append("product_photo",product_photo)
+            //interact with the axios to help you use the method POST
 
-            const response = await axios.post("https://kbenkamotho.alwaysdata.net/api/addproducts", formdata);
+            const response = await axios.post("https://glimmer.alwaysdata.net/api/add_product", formdata);
 
-            setLoading("");
-              // Check if the server actually returned success data
-            if (response.data) {
-               //if succesful,let a person get redirected to another page\
-              
-            } else {
-                setError("credentials are incorrect");
-            }
+            //set loading hook back to default
+
+            setLoading(false);
             
+            // update the success hook with a message
 
-        }
+          setSuccess(response.data.message)
 
-        catch(error){
- setLoading("");
-            // Capture specific server error if available, else generic error
-            const serverMsg = error.response?.data?.message || "oops, try again";
-            setError(serverMsg);
+          //clearing the hooks-setting them to default
+          setProductname("");
+          setProductdescription("");
+          setProductcost("");
+          setProductphoto("");
+
+        e.target.reset()
+
+          setTimeout(() => {
+        setSuccess("");
+      }, 5000);
+            } 
+ 
+            catch(error){
+                //set loading hook back to default
+ setLoading(false);
+
+ //update the setError with a message
+ setError(error.message)
+           
         }
     }
     return(
@@ -63,8 +76,13 @@ const Addproducts= () =>{
             <div className="col-md-6 p-4 card shadow">
 <h3>welcome to add product</h3>
 
-{/* bind the loading  hook */}
+ {/* bind the loading  hook (7) */}
+ 
 {loading && <Loader/>}
+
+                {success && <h3 className="text-success">{success}</h3>}
+                {error && <h4 className="text-danger">{error}</h4>}
+ 
   <form onSubmit={handleSubmit}>
 
 <input type="text"
@@ -73,6 +91,7 @@ className="form-control"
 required
 value={product_name}
 onChange={(e)=>setProductname(e.target.value)} />
+
  <br />
 
 <input type="text"
@@ -81,6 +100,8 @@ className="form-control"
 required
 value={product_description}
 onChange={(e)=>setProductdescription(e.target.value)} />
+
+{/* {product_description} */}
  <br />
 
 <input type="number"
@@ -92,12 +113,14 @@ onChange={(e) =>setProductcost(e.target.value)}/> <br />
 
 {/* {product_cost} */}
 
+{/* {product_cost} */}
+
 <label className="text-primary">product photo</label>
 <input type="file"
 className="form-control"
 required
-accept="image/*"
-onChange={(e) => setProductphoto(e.target.files[0])}/> <br />
+accept="/*"
+onChange={(e) =>setProductphoto(e.target.files[0])}/> <br />
 
 
 <input type="submit"
--- sokogardenapp/src/components/Getproducts.jsx
+++ sokogardenapp/src/components/Getproducts.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Loader from './Loader';
 
 const Getproducts = () => {
-  return (
-    <div class="Getproducts">Welcome to Get products</div>
-  )
-}
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-export default Getproducts
+//base image url
+const img_url ="https://glimmer.alwaysdata.net/static/images/"
+
+  const fetchProducts = async () => {
+    try {
+      setLoading(true);
+
+      const response = await axios.get("https://glimmer.alwaysdata.net/api/get_products");
+      setProducts(response.data);
+
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className='row'>
+      <div className="h3 text-primary">available products</div>
+
+      {loading && <Loader />}
+      {error && <h4 className='text-danger'>{error}</h4>}
+
+      {/* map the products */}
+      {products.map((product) => (
+        <div key={product.id} className="div col-md-3 justify-content-center product_img">
+          <div className="div card shadow product_img mt-3">
+            <img 
+            src={img_url+product.product_photo}
+             alt="product name" />
+            <div className="card-body mt-3 ">
+              <div className="text-primary">{product.product_name}</div>
+              <div className="text-dark">{product.product_description.slice(0,100)}...</div>
+              <div className="text-warning">kes {product.product_cost}</div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Getproducts;
